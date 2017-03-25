9df75426a0005ea99e46b9748a083cbda0254b94
Marko Nikic <darkblue.azurite@gmail.com>
2017-03-25 17:24:48 +0100

    stylize trade and brush up error message display

--- client/js/components/Trade/index.js
+++ client/js/components/Trade/index.js
@@ -1,6 +1,6 @@
 const React = require("react");
 const { connect } = require("react-redux");
-const { Grid, Row, Col, Button } = require("react-bootstrap");
+const { Grid, Row, Col, Button, Image } = require("react-bootstrap");
 const { object, func } = React.PropTypes;
 const Book = require("../Book");
 
@@ -22,7 +22,7 @@ const Trade = React.createClass({
     negotiateTrade: func.isRequired
   },
   render: function() {
-    let { other_stage, other_library, self_stage, self_library } = this.props.trade;
+    let { other_stage, other_library, self_stage, self_library, other, self, trade_request } = this.props.trade;
     let {
       router,
       addToSelfStage,
@@ -34,9 +34,12 @@ const Trade = React.createClass({
       declineTrade,
       negotiateTrade
     } = this.props;
+
+    if(!self) { self = {}; }
+    if(!other) { other = {}; }
     return(
       <Grid className="mainGrid" fluid>
-        <Row>
+        <Row className={styles.tradeBackGround}>
           <Col sm={6} xs={12} className={styles.stage}>
             {other_stage.map((book) => {
               return(
@@ -48,21 +51,40 @@ const Trade = React.createClass({
                 />
               );
             })}
+            {
+              other_stage.length === 0 &&
+              <p className={styles.stageDesc}>Your Demands</p>
+            }
           </Col>
           <Col sm={6} xs={12} className={styles.library}>
-            {other_library.map((book) => {
-              return(
-                <Book
-                  key={book.id}
-                  data={book}
-                  type="add"
-                  action={addToOtherStage}
-                />
-              );
-            })}
+            <div className={styles.user}>
+              <Image
+                src={other.image_url}
+                className={styles.userImg}
+                responsive
+                circle
+              />
+              <span className={styles.userLib}>
+                {other.username + "'s Library"}
+              </span>
+            </div>
+            <hr style={{color: "#dddddd"}}/>
+            <div>
+              {other_library.map((book) => {
+                return(
+                  <Book
+                    key={book.id}
+                    data={book}
+                    type="add"
+                    action={addToOtherStage}
+                  />
+                );
+              })}
+            </div>
           </Col>
         </Row>
-        <Row>
+        <div className={styles.divider}/>
+        <Row className={styles.tradeBackGround}>
           <Col sm={6} xs={12} className={styles.stage}>
             {self_stage.map((book) => {
               return(
@@ -74,40 +96,81 @@ const Trade = React.createClass({
                 />
               );
             })}
+            {
+              self_stage.length === 0 &&
+              <p className={styles.stageDesc}>Your Offer</p>
+            }
           </Col>
           <Col sm={6} xs={12} className={styles.library}>
-            {self_library.map((book) => {
-              return(
-                <Book
-                  key={book.id}
-                  data={book}
-                  type="add"
-                  action={addToSelfStage}
-                />
-              );
-            })}
+            <div className={styles.user}>
+              <Image
+                src={self.image_url}
+                className={styles.userImg}
+                responsive
+                circle
+              />
+              <span className={styles.userLib}>
+                Your Library
+              </span>
+            </div>
+            <hr style={{color: "#dddddd"}}/>
+            <div>
+              {self_library.map((book) => {
+                return(
+                  <Book
+                    key={book.id}
+                    data={book}
+                    type="add"
+                    action={addToSelfStage}
+                  />
+                );
+              })}
+            </div>
           </Col>
         </Row>
-        <Row>
+        <div className={styles.divider}/>
+        {
+          trade_request.error &&
+          <Row>
+            <Col xs={12}>
+              <p className="error-msg">
+                {trade_request.error.message}
+              </p>
+            </Col>
+          </Row>
+        }
+        <Row className={styles.actionBtns}>
           {
             router.location.pathname === "/trade/new" &&
-            <Button className="btnInverse" onClick={initiateTrade}>
-              Initiate Trade
-            </Button>
+            <Col xs={12} className="text-center">
+              <Button bsStyle="info" onClick={initiateTrade} disabled={trade_request.isPending}>
+                {trade_request.isPending ? "Processing..." : "Initiate Trade"}
+              </Button>
+            </Col>
           }
           {
             router.location.pathname === "/trade" &&
-            <div>
-              <Button className="btnInverse" onClick={declineTrade}>
-                Decline Trade
+            <Col xs={4} className="text-left">
+              <Button bsStyle="danger" onClick={declineTrade} disabled={trade_request.isPending}>
+                {trade_request.isPending ? "Processing..." : "Decline"}
               </Button>
-              <Button className="btnInverse" onClick={negotiateTrade}>
-                negotiate Trade
+            </Col>
+          }
+          {
+            router.location.pathname === "/trade" &&
+            <Col xs={4} className="text-center">
+              <Button bsStyle="info" onClick={negotiateTrade} disabled={trade_request.isPending}>
+                {trade_request.isPending ? "Processing..." : "Negotiate"}
               </Button>
-              <Button className="btnInverse" onClick={acceptTrade}>
-                Accept Trade
+            </Col>
+          }
+          {
+            router.location.pathname === "/trade" &&
+            <Col xs={4} className="text-right">
+              <Button bsStyle="success" onClick={acceptTrade} disabled={trade_request.isPending}>
+                {trade_request.isPending ? "Processing..." : "Accept"}
               </Button>
-            </div>
+            </Col>
           }
         </Row>
       </Grid>
